f6f67e4eb7c6788597a0a762e2c63d978a2002c2
Gustavo <gustavo.prog40@gmail.com>
2021-09-01 13:46:11 -0300

    leyaout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,109 +13,116 @@ import {
 } from './styles'
 import SideBarLeftmenu from '../../components/Sidebar/Sidebar'
 
-const movies = [
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-  {
-    title: 'o Titulo',
-    image: 'https://source.unsplash.com/random',
-  },
-]
+// const movies = [
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+//   {
+//     title: 'o Titulo',
+//     image: 'https://source.unsplash.com/random',
+//   },
+// ]
+
+// interface IProducts {
+//   _id: string
+//   title: string
+//   price: number
+//   description: string
+//   image: string
+// }
 
 export default function Home() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [productsList, setProductsList] = useState([])
-
+  const [productsList, setProductsList]: any = useState([])
   async function getAllProducts() {
     const { data } = await api.get('/')
     setProductsList(data)
@@ -132,8 +139,28 @@ export default function Home() {
       <Content>
         <SideBarLeftmenu />
         <ListMovies>
-          {movies.map((movie) => {
-            return <Card image={movie.image} />
+          {productsList.map((item: any) => {
+            return (
+              <div
+                key={item._id}
+                style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+              >
+                <Card image={`https://imas-api.herokuapp.com/files/${item.image}`} />
+                <p
+                  style={{
+                    fontSize: '15px',
+                    color: '#d9d9d9',
+                    fontWeight: 'bold',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                  }}
+                >
+                  {item.title}
+                  <span>R$ {item.price}</span>
+                </p>
+              </div>
+            )
             // return <Card image={`https://ecomerce-api.herokuapp.com/files/${movie.image}`} />
           })}
         </ListMovies>
